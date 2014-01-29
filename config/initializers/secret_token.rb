d6a1b51ad187e866f73f9e0ffd90279b633d00c9
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-01-28 20:49:56 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lindsterest::Application.config.secret_key_base = 'fcbe493c51152a940d2f2c428b1c49416c0014e008939cc15c2561b8f7658724e875f11d2d1626c8be4dce8677509cb6461a651454bbb8663efb432613141629'
